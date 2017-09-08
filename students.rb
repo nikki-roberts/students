fea179093d2577da0d987691f04d9acb94714a55
Nikki Roberts <robertsni@s.dcsdk12.org>
2017-09-08 10:43:01 -0600

    initial commit

---
+++ students.rb
@@ -0,0 +1,14 @@
+students = []
+eye_colors = []
+ages = []
+
+File.open("student_data.csv").each do |line|
+	info = line.split(",")
+	students.push(info[0].strip)
+	eye_colors.push(info[1].strip)
+	ages.push(info[2].strip.to_i)
+end
+
+print students, "\n"
+print eye_colors, "\n"
+print ages, "\n"
